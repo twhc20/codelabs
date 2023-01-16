fe2540f1eaecdb3bc3cc38415b01abd03a265b28
twhc20 <twhc20@bath.ac.uk>
2023-01-16 19:15:22 +0000

    Losing the game

--- flame-building-doodle-dash/step_02/lib/game/managers/object_manager.dart
+++ flame-building-doodle-dash/step_02/lib/game/managers/object_manager.dart
@@ -75,6 +75,7 @@ class ObjectManager extends Component with HasGameRef<DoodleDash> {
 
       _cleanupPlatforms();
       // Losing the game: Add call to _maybeAddEnemy()
+      _maybeAddEnemy();
       // Powerups: Add call to _maybeAddPowerup();
     }
 
@@ -108,6 +109,9 @@ class ObjectManager extends Component with HasGameRef<DoodleDash> {
         break;
       case 2:
         enableSpecialty('broken');
+        break;
+      case 5:
+        enableSpecialty('enemy');
         break;
     }
   }
@@ -172,7 +176,33 @@ class ObjectManager extends Component with HasGameRef<DoodleDash> {
     }
     return NormalPlatform(position: position);
   }
+
   // Losing the game: Add enemy code
+  final List<EnemyPlatform> _enemies = [];
+  void _maybeAddEnemy() {
+    if (specialPlatforms['enemy'] != true) {
+      return;
+    }
+    if (probGen.generateWithProbability(20)) {
+      var enemy = EnemyPlatform(
+        position: Vector2(_generateNextX(100), _generateNextY()),
+      );
+      add(enemy);
+      _enemies.add(enemy);
+      _cleanupEnemies();
+    }
+  }
+
+  void _cleanupEnemies() {
+    final screenBottom = gameRef.player.position.y +
+        (gameRef.size.x / 2) +
+        gameRef.screenBufferSpace;
+
+    while (_enemies.isNotEmpty && _enemies.first.position.y > screenBottom) {
+      remove(_enemies.first);
+      _enemies.removeAt(0);
+    }
+  }
 
   // Powerups: Add Power-Up code
 }
